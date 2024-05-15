2ef0617daf57aebfb6ecc8751687591fc37534d3
SuguruIrie3224 <121938178+SuguruIrie3224@users.noreply.github.com>
2024-05-15 22:27:22 +0900

    Revert "feat: Add modules for register, opcode, rom, and port"
    This reverts commit b5c141e2b1dc6fa391bacb43ccac364bad1fae0b.

--- app/src/lib.rs
+++
@@ -1,8 +0,0 @@
-/// ここで他のファイルにアクセスするために、`mod`を使って他のファイルを読み込む
-
-pub mod register;
-pub mod opcode;
-pub mod rom;
-pub mod port;
-
-pub mod cpu;
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,37 +1,3 @@
-use app::cpu::Cpu;
-
-fn main(){
-    println!("Hello, World!");
-
-    let mut cpu = Cpu::new();
-
-    let program: Vec<u8> = vec![
-        0b_1011_0011,
-        0b_1011_0110,
-        0b_1011_1100,
-        0b_1011_1000,
-
-        0b_1011_1000,
-        0b_1011_1100,
-        0b_1011_0110,
-        0b_1011_0011,
-
-        0b_1011_0001,
-        0b_1111_0000,
-
-    ];
-
-    cpu.load(program);
-
-    loop {
-        let data = cpu.fetch();
-        let (opcode, operand) = cpu.decode(data);
-        cpu.execute(opcode, operand);
-
-        // Outputポートを2進数の4桁で表示する
-        println!("{:04b}", cpu.output());
-
-        // 1秒まつ
-        std::thread::sleep(std::time::Duration::from_secs(1));
-    }
+fn main() {
+    println!("Hello, world!");
 }
